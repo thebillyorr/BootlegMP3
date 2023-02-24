f2eabb5b79d6ed0b125c28bb3360d38510c19a02
Billy Orr <thebillyorr@gmail.com>
2023-02-23 23:43:25 -0800

    New Songs!!

--- bootlegMP3.js
+++ bootlegMP3.js
@@ -15,6 +15,10 @@ let songDatabase = {
         "Album": "Lycoris Recoil",
         "URL": "https://drive.google.com/file/d/1QenDRXyv6UCNSAvFJlozrJF9iHYKcfGz/view?usp=share_link"
     },
+    "Main Theme": {
+        "Album": "Love Live! Superstar!!",
+        "URL": "https://drive.google.com/file/d/1PCQ_NxW7ci7AAD8pOfshIg6QbRJA3mZ_/view?usp=share_link"
+    },
     "始まりの瞬間": {
         "Album": "Love Live! Superstar!!",
         "URL": "https://drive.google.com/file/d/13GRT7kmFGmK3ef2pzzcs3JCP_E7INCd-/view?usp=share_link"
@@ -22,6 +26,234 @@ let songDatabase = {
     "想いをカタチに": {
         "Album": "Love Live! Superstar!!",
         "URL": "https://drive.google.com/file/d/1HPtMgc0DahpuZMtjtUHZP-KoxV165lGF/view?usp=share_link"
+    },
+    "星の連なり": {
+        "Album": "Love Live! Superstar!!",
+        "URL": "https://drive.google.com/file/d/11GEp84tRozXbc6brQl-7f8rpM0ZjptQq/view?usp=share_link"
+    },
+    "特訓スタート！": {
+        "Album": "Love Live! Superstar!!",
+        "URL": "https://drive.google.com/file/d/174YYGd-2OX8Yqxf7BJ873qQ2tkSZZWvh/view?usp=share_link"
+    },
+    "輝く気持ち": {
+        "Album": "Love Live! Superstar!!",
+        "URL": "https://drive.google.com/file/d/1iHogmnYsxhZ8zmrt6tA3Y5pOZxLPCVPZ/view?usp=share_link"
+    },
+    "高まり" : {
+        "Album": "Love Live! Superstar!!",
+        "URL": "https://drive.google.com/file/d/152bssd59MAJ1AMwvBHmIJrx5hfMIbrmv/view?usp=share_link"
+    },
+    "Calm and Healthy": {
+        "Album": "Akebi's Sailor Uniform",
+        "URL": "https://drive.google.com/file/d/1o49W5g1tLXXSbOiI--kF_bjiZUFBBO7O/view?usp=share_link"
+    },
+    "Exciting": {
+        "Album": "Akebi's Sailor Uniform",
+        "URL": "https://drive.google.com/file/d/17fhJz7M-tPyXvhxhZ5gKGfXhwbOxe90R/view?usp=share_link"
+    },
+    "Komichi's Theme": {
+        "Album": "Akebi's Sailor Uniform",
+        "URL": "https://drive.google.com/file/d/1G9bO7u2XkBSm12FSTYAjk6fcsvm2CXqb/view?usp=share_link"
+    },
+    "My First Sailor Uniform": {
+        "Album": "Akebi's Sailor Uniform",
+        "URL": "https://drive.google.com/file/d/1HtiRHunoETrigqwK07l_KQZNAyz1RqSI/view?usp=share_link"
+    },
+    "Start Running, Komichi": {
+        "Album": "Akebi's Sailor Uniform",
+        "URL": "https://drive.google.com/file/d/1Ly27BlM4nqLRoIaMcYRmbGxl05Ad160L/view?usp=share_link"
+    },
+    "You Can Make Lots of Friends!": {
+        "Album": "Akebi's Sailor Uniform",
+        "URL": "https://drive.google.com/file/d/1_qF5wEaF4q4F_WmfHEAi0zzjwXTd9ODY/view?usp=share_link"
+    },
+    "Accomplishment": {
+        "Album": "Girlfriend, Girlfriend",
+        "URL": "https://drive.google.com/file/d/1MC5XEBRdOKqKh-OE3bOT4pqtdg-ktxqY/view?usp=share_link"
+    },
+    "Daydream": {
+        "Album": "Girlfriend, Girlfriend",
+        "URL": "https://drive.google.com/file/d/1FZU0xhwnqVk5obrrxuhtfDchKP4dd1eG/view?usp=share_link"
+    },
+    "Fluttering Heart": {
+        "Album": "Girlfriend, Girlfriend",
+        "URL": "https://drive.google.com/file/d/1OQ4Ecqs01X0Q_tUCZxXy4W991udnOmal/view?usp=share_link"
+    },
+    "Girlfriend, Girlfriend": {
+        "Album": "Girlfriend, Girlfriend",
+        "URL": "https://drive.google.com/file/d/1gK1wnIPCvK7wbg9uUXRT3MpKnehwGWaF/view?usp=share_link"
+    },
+    "Lovely Girls": {
+        "Album": "Girlfriend, Girlfriend",
+        "URL": "https://drive.google.com/file/d/1v2Hodq076TfYyChFkhu7E4nrYsExox4k/view?usp=share_link"
+    },
+    "Please Go Out With Me!": {
+        "Album": "Girlfriend, Girlfriend",
+        "URL": "https://drive.google.com/file/d/1kK3-WaYjatfkUyzm-WDD9jvfAmSm7dXO/view?usp=share_link"
+    },
+    "Spring Air": {
+        "Album": "Girlfriend, Girlfriend",
+        "URL": "https://drive.google.com/file/d/1jHplo59w-e4TzA5xmNeOYi1bA1Yn7jSr/view?usp=share_link"
+    },
+    "Strong Will": {
+        "Album": "Girlfriend, Girlfriend",
+        "URL": "https://drive.google.com/file/d/1etB9StYrmMs5eGWqcr4pLf-h-HlyTO5-/view?usp=share_link"
+    },
+    "The End Of Love": {
+        "Album": "Girlfriend, Girlfriend",
+        "URL": "https://drive.google.com/file/d/16ndAmgrlKHpxU_kd6PHPFOfr0x5lEsh2/view?usp=share_link"
+    },
+    "あなたを待っています": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/1npLO-gh2j18ucIDnVKpIcOY7M_TqHgSU/view?usp=share_link"
+    },
+    "フータローを信じる": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/1ZEr3bUXPLUSX_xcpnS46UHZB73ErPJmU/view?usp=share_link"
+    },
+    "やるからには徹底的に": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/1Fo96wWCkihxjcJQvOsSU19PsUwRbVFq9/view?usp=share_link"
+    },
+    "五つ子ゲームファイナル": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/1609A-wKt8GKPMlEMNpzgt1bl27Lc4t3k/view?usp=share_link"
+    },
+    "五等分": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/13JX8sBYNUdGwLZV6QOB2D8y1H64YDae3/view?usp=share_link"
+    },
+    "何度も、何度も": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/1NJ8EY2gtte8KfkZd486G_gb-bvjIKulT/view?usp=share_link"
+    },
+    "変わらないみんな": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/10gO0V9xkUjD4h4yBzx5itZx0hri9R8Qx/view?usp=share_link"
+    },
+    "夢を見ていた": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/13_naAEO9PTZH_shFpOr4HeauadQn4wGl/view?usp=share_link"
+    },
+    "夢を見ていた": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/1Mozl75v8gzbu2dHS50son--K56FBeQNS/view?usp=share_link"
+    },
+    "夢を見ていた": {
+        "Album": "Quintessential Quintuplets",
+        "URL": "https://drive.google.com/file/d/1AysTcv29xjO9w93AFp8MbfKeDCzIECWy/view?usp=share_link"
+    },
+    "Makutosoke, Nothing to worry about": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-m4-aMxkpFpzZtsrrXXsQHAIi90Ld2s-/view?usp=share_link"
+    },
+    "A New Start": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1PptzrRwV2u_9eNwRBgM6E8GfVhygFugg/view?usp=share_link"
+    },
+    "Kukuru": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1Pn3A7TdWVG6w_EpUoNDzxTcojclmn89q/view?usp=share_link"
+    },
+    "Let Me Be Led": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1zMpHKwA7KVUTNPDWdqi5f84l0RZ2rtE7/view?usp=share_link"
+    },
+    "Aquatope": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-Cv6mEPJma1rJcN8KirwOyc4fAVCWl_2/view?usp=share_link"
+    },
+    "Acrylic Glass": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1e4e7h4FBFs_laRQbLPXDeDKzt357QKSD/view?usp=share_link"
+    },
+    "Recreation": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-FRXvV41maZHvOFhzk8dsqBWl6j0M1xY/view?usp=share_link"
+    },
+    "Because You're Here": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-DfPbTzMRPAKcfpwiO6xzmz_xwmSd-Wc/view?usp=share_link"
+    },
+    "Re_Start!": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-NxDhsfCp-8I6dH2NfS7M3sgmUM1tAyR/view?usp=share_link"
+    },
+    "Matching Key Chain": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-L58BTy7HZB7i4kUJN5WeMESxE1wOaWl/view?usp=share_link"
+    },
+    "Aqua Sphere": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-KKgZ5m7s6L6zpi2o4NT4BzqggnpIAmF/view?usp=share_link"
+    },
+    "Touch Pool": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-GLsMyU4HS7SOflGrbtv8c8IXYOEn4r8/view?usp=share_link"
+    },
+    "Aquarium Tingara": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-QG-nma25tvcRgKrtM74xopLxARwXKEM/view?usp=share_link"
+    },
+    "I'm Not Crying": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-PLveki3LexFIpYkBX60xOsuWGM9QAy5/view?usp=share_link"
+    },
+    "Natsuyasumi Kancho": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-VAsqWdvOD6gWQ4LT2wODRR_PhlUDer-/view?usp=share_link"
+    },
+    "The End and Beginning of a Dream": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-o-WiLynUBD9MfG5NNrMWQnGWabYHXLy/view?usp=share_link"
+    },
+    "A Fresh Start For The Two Of Us": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-j6YuGdjffabOpeeW4HS47FkHFI7TIxc/view?usp=share_link"
+    },
+    "First Penguin": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-Vqcgd2J6dXDDzQeco-TW-gttEA7hFRI/view?usp=share_link"
+    },
+    "To Protect A Dream": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-VhSOax_DBb2GV7HZTiduKW4YZmWdO0y/view?usp=share_link"
+    },
+    "Into The Future": {
+        "Album": "The Aquatope on White Sand",
+        "URL": "https://drive.google.com/file/d/1-ob53CU09NplM5hIEEYFS5PRtuYi_itG/view?usp=share_link"
+    },
+    "出会い": {
+        "Album": "Tada Never Falls in Love",
+        "URL": "https://drive.google.com/file/d/16bSmeDDz1Vah3be84K9PhZqzdTkVe6zm/view?usp=share_link"
+    },
+    "充実した時間": {
+        "Album": "Tada Never Falls in Love",
+        "URL": "https://drive.google.com/file/d/1jAWwjbWq704vi_3xPn3SJ0aBdhAJTcw5/view?usp=share_link"
+    },
+    "懐かしい思い出": {
+        "Album": "Tada Never Falls in Love",
+        "URL": "https://drive.google.com/file/d/1yb8dkww8OXds_8iCifxixLCjsq2vmsqX/view?usp=share_link"
+    },
+    "恋は秒": {
+        "Album": "Tada Never Falls in Love",
+        "URL": "https://drive.google.com/file/d/1-AxatlzO6mVaov62FPFiS-cPQtWpHrpk/view?usp=share_link"
+    },
+    "ダンスパーティ": {
+        "Album": "Tada Never Falls in Love",
+        "URL": "https://drive.google.com/file/d/11_CuqscG4kRlRF2g2QNf_z625ppwUF9L/view?usp=share_link"
+    },
+    "珈琲でもいかが": {
+        "Album": "Tada Never Falls in Love",
+        "URL": "https://drive.google.com/file/d/1x0c-RxIbZdx0FlqZEKd7-WUnht4AWiE6/view?usp=share_link"
+    },
+    "シャルルとテレサ": {
+        "Album": "Tada Never Falls in Love",
+        "URL": "https://drive.google.com/file/d/1OW2iOjTlhWIkFEm-GpuHOEIqKUAi-Dhb/view?usp=share_link"
+    },
+    "王子様の登場!": {
+        "Album": "Tada Never Falls in Love",
+        "URL": "https://drive.google.com/file/d/1z6qHv6-0cENcf1oyyJCx9_Lyt6m0KPP4/view?usp=share_link"
     },
 }
 
